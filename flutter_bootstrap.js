8140ece954e484fc7ef18867dda752c7a187e321
Chain <chenlichong813@gmail.com>
2026-05-25 21:39:39 +0800

    Deploy latest offline mode and pairing features to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3249770130" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3094010430" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
