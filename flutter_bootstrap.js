153c24ae289850503ec12314fda87aff96aafcc4
Chain <chenlichong813@gmail.com>
2026-05-25 22:00:36 +0800

    Update login hint to Account and add Nickname setting in PairScreen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3094010430" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2846034434" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
